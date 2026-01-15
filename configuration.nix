780a8536bc14748bfb5f674d4e0acfbabca92f35
tempz-miguelAndrade <miguelandradedesouzaifc@exemplo.com>
2026-01-15 16:12:13 -0300

    ajeitando

--- configuration.nix
+++ configuration.nix
@@ -1,38 +1,28 @@
 
  { config, pkgs, ... }: { 
 
-    
-
     imports = [ ./hardware-configuration.nix ./programas.nix ./visual.nix ];
 
 
     # Bootloader (Grub)
-
     boot.loader.systemd-boot.enable = false; 
 
     boot.loader.efi.canTouchEfiVariables = true;
 
-
     boot.loader.grub = {
-
         enable = true;
-
         device = "nodev";
-
         efiSupport = true;
-
         useOSProber = true;
 
     };
 
 
     # Kernel mais recente
-
     boot.kernelPackages = pkgs.linuxPackages_latest;
 
 
     # Rede e Hardware
-
     networking.hostName = "nixos-tempz";
 
     networking.networkmanager.enable = true;
@@ -41,7 +31,6 @@
 
 
     # Otimizações
-
     nix.settings.download-buffer-size = 250000000;
 
     nix.settings.max-jobs = 1;
@@ -50,49 +39,34 @@
 
 
     # Teclado e Idioma
-
     console.keyMap = "br-abnt2";
 
     i18n.defaultLocale = "pt_BR.UTF-8";
 
-
     services.xserver.xkb = {
-
         layout = "br";
-
         variant = "";
-
     };
 
 
     # Usuário
-
     users.users.tempz = {
-
         isNormalUser = true;
-
         extraGroups = [ "wheel" "networkmanager" "video" ];
-
     };
 
 
     # Áudio e Vídeo
-
     services.pipewire = {
-
         enable = true;
-
         alsa.enable = true;
-
         pulse.enable = true;
-
     };
 
     hardware.graphics.enable = true;
 
 
     # Interface COSMIC
-
     services.desktopManager.cosmic.enable = true;
 
     services.displayManager.cosmic-greeter.enable = true;
@@ -101,7 +75,6 @@
 
 
     # Versão do sistema (Não altere)
-
     system.stateVersion = "24.11";
 
 } 
